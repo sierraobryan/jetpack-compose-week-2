2b4483d5038385779892eb3784555782d62e7681
Sierra Obryan <sierra@atomicrobot.com>
2021-03-03 21:14:34 -0500

    first pass at a count down timer!

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -17,20 +17,36 @@ package com.example.androiddevchallenge
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyTheme {
-                MyApp()
+                MyApp(viewModel)
             }
         }
     }
@@ -38,24 +54,74 @@ class MainActivity : AppCompatActivity() {
 
 // Start building your app here!
 @Composable
-fun MyApp() {
+fun MyApp(mainViewModel: MainViewModel, modifier: Modifier = Modifier) {
+
+    val seconds = mainViewModel.seconds.observeAsState()
+    val minutes = mainViewModel.minutes.observeAsState()
+    val hours = mainViewModel.hours.observeAsState()
+    val running = mainViewModel.running.observeAsState()
+
     Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
+        Column() {
+            Text(text = "Ready... Set... GO!")
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(40.dp)
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TimeColumn(value = hours.value, enabled = running.value == false) { mainViewModel.modifyTime(TimeUnit.HOUR, it) }
+                Text(text = " : ", fontSize = 32.sp)
+                TimeColumn(value = minutes.value, enabled = running.value == false) { mainViewModel.modifyTime(TimeUnit.MIN, it) }
+                Text(text = " : ", fontSize = 32.sp)
+                TimeColumn(value = seconds.value, enabled = running.value == false) { mainViewModel.modifyTime(TimeUnit.SEC, it) }
+            }
+            Row(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 40.dp, end = 40.dp,)
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = { mainViewModel.startCountDown() }) {
+                    Text(text = "Start")
+                }
+                Button(onClick = { mainViewModel.cancel() }) {
+                    Text(text = "Cancel")
+                }
+            }
+        }
     }
 }
 
-@Preview("Light Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun LightPreview() {
-    MyTheme {
-        MyApp()
+fun TimeColumn(value: Int?, enabled: Boolean, function: (Operation) -> Unit) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IncrementButton(Operation.ADD, enabled = enabled, function = function)
+        Text(text = String.format("%02d", value ?: 0), fontSize = 32.sp)
+        IncrementButton(Operation.SUBTRACT, enabled = enabled, function = function)
     }
 }
 
-@Preview("Dark Theme", widthDp = 360, heightDp = 640)
 @Composable
-fun DarkPreview() {
-    MyTheme(darkTheme = true) {
-        MyApp()
+fun IncrementButton(operation: Operation, enabled: Boolean, function: (Operation) -> Unit) {
+    Button(
+        onClick = { function.invoke(operation) },
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = MaterialTheme.colors.background,
+            disabledBackgroundColor = MaterialTheme.colors.background
+        ),
+        elevation = ButtonDefaults.elevation(defaultElevation = 0.dp)
+    ) {
+        Text(
+            text = if (operation == Operation.ADD) "++" else "--",
+            fontWeight = FontWeight.Bold, fontSize = 24.sp
+        )
     }
 }
---
+++ app/src/main/java/com/example/androiddevchallenge/MainViewModel.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androiddevchallenge
+
+import android.os.CountDownTimer
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+enum class Operation {
+    ADD, SUBTRACT
+}
+
+enum class TimeUnit {
+    SEC, MIN, HOUR
+}
+
+class MainViewModel : ViewModel() {
+
+    private var pauseTimer: CountDownTimer? = null
+
+    val seconds = MutableLiveData(10)
+    val minutes = MutableLiveData(1)
+    val hours = MutableLiveData(0)
+    val running = MutableLiveData(false)
+
+    fun startCountDown() {
+        cancel()
+        pauseTimer = object : CountDownTimer((getTimeInSecond() * 1000).toLong(), 1000) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                val hourTime = (millisUntilFinished / 1000 / 60 / 60).toInt()
+                val minuteTime = (millisUntilFinished / 1000 / 60 % 60).toInt()
+                val secondTime = (millisUntilFinished / 1000 % 60).toInt()
+                if (secondTime != seconds.value) {
+                    seconds.postValue(secondTime)
+                }
+                if (minuteTime != minutes.value) {
+                    minutes.postValue(minuteTime)
+                }
+                if (hourTime != hours.value) {
+                    hours.postValue(hourTime)
+                }
+            }
+
+            override fun onFinish() {
+                seconds.postValue(10)
+            }
+        }
+        pauseTimer?.start()
+        running.postValue(true)
+    }
+
+    fun cancel() {
+        pauseTimer?.cancel()
+        running.postValue(false)
+    }
+
+    fun modifyTime(timeUnit: TimeUnit, operation: Operation) {
+        when (timeUnit) {
+            TimeUnit.SEC -> seconds.postValue(
+                operation(seconds.value ?: 0, operation).coerceIn(0, 59)
+            )
+            TimeUnit.MIN -> minutes.postValue(
+                operation(minutes.value ?: 0, operation).coerceIn(0, 59)
+            )
+            TimeUnit.HOUR -> hours.postValue(
+                operation(hours.value ?: 0, operation).coerceIn(0, 99)
+            )
+        }
+    }
+
+    private fun operation(currentValue: Int, operation: Operation): Int {
+        return when (operation) {
+            Operation.ADD -> currentValue + 1
+            Operation.SUBTRACT -> currentValue - 1
+        }
+    }
+
+    private fun getTimeInSecond() = ((hours.value ?: 0) * 60 * 60) + ((minutes.value ?: 0) * 60) + (seconds.value ?: 0)
+}
